36c58f186e944a4fe85a10efdcefc612e0d08bf2
gbarnacle79 <92514175+gbarnacle79@users.noreply.github.com>
2022-01-17 16:18:56 +0000

    Updated rest.tf to include all VM's required

--- rest.tf
+++ rest.tf
@@ -2,7 +2,19 @@ provider "aws" {
     version                 = "~> 2.0"
     region                  = "eu-west-2"
 }
-resource "aws_instance" "EC2" {
+resource "aws_instance" "nginx-VM" {
+    ami           = "ami-0f9124f7452cdb2a6"
+    instance_type = "t2.micro"
+    key_name      = "MyKeyPai"
+    vpc_security_group_ids = ["pet-sg", "ssh-sg", "80-sg", "all-sg"]
+}
+resource "aws_instance" "worker-VM" {
+    ami           = "ami-0f9124f7452cdb2a6"
+    instance_type = "t2.micro"
+    key_name      = "MyKeyPai"
+    vpc_security_group_ids = ["pet-sg", "ssh-sg", "80-sg", "all-sg"]
+}
+resource "aws_instance" "manager-VM" {
     ami           = "ami-0f9124f7452cdb2a6"
     instance_type = "t2.micro"
     key_name      = "MyKeyPai"
