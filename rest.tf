9294f042198b368312cbf7f4cb28bbb860ced4f5
gbarnacle79 <92514175+gbarnacle79@users.noreply.github.com>
2022-01-17 15:28:47 +0000

    Update rest.tf

--- rest.tf
+++ rest.tf
@@ -1,7 +1,8 @@
 provider "aws" {
     version                 = "~> 2.0"
     region                  = "eu-west-2"
-    shared_credentials_file = "~/.aws/credentials"
+    aws_access_key_id = "${AWS_ACCESS_KEY_ID}"
+    aws_secret_acess_key = "${AWS_SECRET_ACCESS_KEY}"
 }
 resource "aws_instance" "EC2" {
     ami           = "ami-0f9124f7452cdb2a6"
