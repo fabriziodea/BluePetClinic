990b285c78564384318cf3033e5b3cf5b7cc9eb9
gbarnacle79 <92514175+gbarnacle79@users.noreply.github.com>
2022-01-17 16:20:02 +0000

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,3 +1,9 @@
 output "Public_IP" {
-    value = aws_instance.EC2.public_ip
+    value = aws_instance.nginx-VM.public_ip
+}
+output "Public_IP" {
+    value = aws_instance.worker-VM.public_ip
+}
+output "Public_IP" {
+    value = aws_instance.manager-VM.public_ip
 }
