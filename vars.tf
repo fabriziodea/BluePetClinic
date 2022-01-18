082aa8fddf98e521906781b87170b56638953e95
gbarnacle79 <92514175+gbarnacle79@users.noreply.github.com>
2022-01-18 15:07:11 +0000

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,3 +1,3 @@
-variable "nginx_Public_IP" = {nginx_Public_IP}
-variable "manager_Public_IP" = {manager_Public_IP}
-variable "worker_Public_IP" = {worker_Public_IP}
+variable "nginx_Public_IP"  {nginx_Public_IP}
+variable "manager_Public_IP"  {manager_Public_IP}
+variable "worker_Public_IP"  {worker_Public_IP}
