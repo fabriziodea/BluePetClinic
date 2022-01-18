ee164c02837b4d7ae2eb4615c18af9b39c359038
gbarnacle79 <92514175+gbarnacle79@users.noreply.github.com>
2022-01-18 14:40:35 +0000

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,8 +1,8 @@
 locals {
     nginx = templatefile("${path.module}/template.tpl", {
         nginx_Public_IP = aws_instance.nginx-VM.public_ip, 
-    }
-
+    })
+}
 
 output "nginx_Public_IP" {
     value = aws_instance.nginx-VM.public_ip
@@ -10,14 +10,16 @@ output "nginx_Public_IP" {
 locals {
     worker = templatefile("${path.module}/template.tpl", {
         worker_Public_IP = aws_instance.worker-VM.public_ip, 
-    }
+    })
+}
 output "worker_Public_IP" {
     value = aws_instance.worker-VM.public_ip
 }
 locals {
     manager = templatefile("${path.module}/template.tpl", {
         manager_Public_IP = aws_instance.manager-VM.public_ip, 
-    }
+    })
+}
 output "manager_Public_IP" {
     value = aws_instance.manager-VM.public_ip
 }
