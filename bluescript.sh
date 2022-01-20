5418e23da7828483a383e2f7537c031367cbf575
fabriziodea <92307344+fabriziodea@users.noreply.github.com>
2022-01-20 10:16:24 +0000

    Update bluescript.sh

--- bluescript.sh
+++ bluescript.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 sudo npm install --global yarn
-git clone https://github.com/fabriziodea/angular-cli.git
-cd angular-cli
+git clone https://github.com/fabriziodea/angular-cli.git /home/ubuntu/angular-cli
+cd /home/ubuntu/angular-cli
 yarn
 yarn build —-local
 sudo npm install -g @angular/cli@latest
