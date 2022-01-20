7e7ef36f4423035a50363969039d25a6fa073bb9
gbarnacle79 <92514175+gbarnacle79@users.noreply.github.com>
2022-01-20 12:23:11 +0000

    Update output.tf

--- output.tf
+++ output.tf
@@ -26,8 +26,8 @@ resource "local_file" "tf_ansible_vars_file_new" {
                 ${aws_instance.nginx-VM.public_ip}
                 
       vars:
-        ansible_user: ubuntu
-        ansible_ssh_private_key_file: '~/.ssh/MyKeyPai.pem'
+        ansible_user: jenkins
+        ansible_ssh_private_key_file: /home/jenkins/.ssh/MyKeyPai.pem
         ansible_python_interpreter: "/usr/bin/python3"
         ansible_ssh_common_args: '-o StrictHostKeyChecking=no'
    DOC
