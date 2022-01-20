d653d157ba1cd3561b5bb5a30a77e5fff6716f8f
gbarnacle79 <92514175+gbarnacle79@users.noreply.github.com>
2022-01-20 11:38:12 +0000

    Update output.tf

--- output.tf
+++ output.tf
@@ -13,10 +13,23 @@ output "manager_Public_IP" {
 
 resource "local_file" "tf_ansible_vars_file_new" {
     content = <<-DOC
-    Vars:
-        NGINX_IP: ${aws_instance.nginx-VM.public_ip}
-        Worker_IP: ${aws_instance.worker-VM.public_ip}
-        Manager_IP: ${aws_instance.manager-VM.public_ip}
-    DOC
-  filename = "ansible/tf_ansible_vars_file.yml"
+    all:
+      children:
+          managers:
+             hosts:
+               "{{ Manager_IP }}"
+          workers:
+            hosts:
+              "{{ Worker_IP }}"
+           nginx:
+            hosts:
+                "{{ NGINX_IP }}"
+                
+       vars:
+        ansible_user: ubuntu
+        ansible_ssh_private_key_file: '~/.ssh/MyKeyPai.pem'
+        ansible_python_interpreter: "/usr/bin/python3"
+        ansible_ssh_common_args: '-o StrictHostKeyChecking=no'
+   DOC
+   filename = "ansible/inventory.yaml"
 }
