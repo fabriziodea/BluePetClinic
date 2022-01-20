044b4c288c0243085d0901bd1b92d1fec050b9a3
gbarnacle79 <92514175+gbarnacle79@users.noreply.github.com>
2022-01-20 10:36:53 +0000

    Update rest.tf

--- rest.tf
+++ rest.tf
@@ -6,57 +6,17 @@ resource "aws_instance" "nginx-VM" {
     ami           = "ami-0f9124f7452cdb2a6"
     instance_type = "t2.micro"
     key_name      = "MyKeyPai"
-    vpc_security_group_ids = ["pet-sg", "ssh-sg", "80-sg", "all-sg"]
+    vpc_security_group_ids = ["petclinic-sg"]
 }
 resource "aws_instance" "worker-VM" {
     ami           = "ami-0f9124f7452cdb2a6"
     instance_type = "t2.micro"
     key_name      = "MyKeyPai"
-    vpc_security_group_ids = ["pet-sg", "ssh-sg", "80-sg", "all-sg"]
+    vpc_security_group_ids = ["petclinic-sg"]
 }
 resource "aws_instance" "manager-VM" {
     ami           = "ami-0f9124f7452cdb2a6"
     instance_type = "t2.micro"
     key_name      = "MyKeyPai"
-    vpc_security_group_ids = ["pet-sg", "ssh-sg", "80-sg", "all-sg"]
-}
-resource "aws_security_group" "open-clinic" {
-    name = "pet-sg"
-    ingress {
-
-        from_port = 9966
-        to_port = 9966
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-}
-resource "aws_security_group" "ssh" {
-    name = "ssh-sg"
-    ingress {
-
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-}
-resource "aws_security_group" "p-80" {
-    name = "80-sg"
-    ingress {
-
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-}
-resource "aws_security_group" "outbound" {
-    name = "all-sg"
-    egress {
-
-        from_port = 0
-        to_port = 9999
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+    vpc_security_group_ids = ["petclinic-sg"]
 }
